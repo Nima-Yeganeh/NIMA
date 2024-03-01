57599584ecb725841c9cb72fd599b20d8ab0f809
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-01 08:31:37 +0100

    done

---
+++ proxy/zv2ray-upstream-server.sh
@@ -0,0 +1,42 @@
+
+sleeptime=0
+echo "Started!"
+sleep $sleeptime
+sudo apt update -y
+sudo apt install iftop mtr -y
+sleep $sleeptime
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
+sleep $sleeptime
+sudo apt update -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+python3 --version
+sleep $sleeptime
+sudo apt update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+sudo apt install docker docker-compose docker.io -y
+sudo apt-get update -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# sudo apt install docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker rm -f $(sudo docker ps -q)
+sleep $sleeptime
+cd utils
+sudo bash bbr.sh
+sleep $sleeptime
+cd ..
+cd v2new/v2ray-upstream-server/
+ls -anp
+docker-compose up -d
+docker ps -a
+cd ..
+cd ..
+ls -anp
