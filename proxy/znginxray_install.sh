9e633079651a8e6700d9b43de6b6543edda8be89
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-02 15:47:47 +0100

    done

---
+++ proxy/znginxray_install.sh
@@ -0,0 +1,49 @@
+
+sleeptime=0
+echo "Started!"
+sleep $sleeptime
+sudo apt update -y
+sudo apt install iftop mtr net-tools -y
+sleep $sleeptime
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
+sleep $sleeptime
+sudo apt update -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+python3 --version
+sleep $sleeptime
+sudo apt update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+# if [ ! -e "/usr/share/keyrings/docker-archive-keyring.gpg" ]; then
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# fi
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker docker-compose docker.io -y
+sudo apt-get update -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# sudo apt install docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker rm -f $(sudo docker ps -q)
+sleep $sleeptime
+cd utils
+sudo bash bbr.sh
+sleep $sleeptime
+cd ..
+cd nginxray/
+ls -anp
+docker-compose up -d
+docker ps -a
+sudo bash info.sh
+cd ..
+cd ..
+ls -anp
