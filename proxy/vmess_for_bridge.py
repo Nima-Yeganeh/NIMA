1d5cfdafd02bcb068f8a2dcde7a9e0f43f00dc89
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-07 13:37:34 +0100

    done

--- proxy/vmess_for_bridge.py
+++ proxy/vmess_for_bridge.py
@@ -12,9 +12,9 @@ from pathlib import Path
 
 uuid="a04be4ef-1797-4ca9-a549-4385ce42494c"
 domain="www.digitalocean.com"
-domain="test.nimaxmusic.ir"
+domain="imaginativedomainnamesmaketheinternetinterestingsrv1deservice.uniquedomainnameswithcharacterandstylearecool.site"
 ipaddr=urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
-ipaddr="test.nimaxmusic.ir"
+ipaddr=domain
 zpath="/ws"
 zpath="/"
 ztls="tls"
