d0af4bf906fa8b9893dc3733946a57c592b8b52a
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-07 08:53:57 +0100

    done

--- proxy/vmess_for_bridge.py
+++ proxy/vmess_for_bridge.py
@@ -10,23 +10,11 @@ from pathlib import Path
 from urllib.request import urlopen
 from pathlib import Path
 
-# path = Path(__file__).parent
-
-# config_file = open(str(path.joinpath('v2ray/config/config.json')), 'r', encoding='utf-8')
-# config = json.load(config_file)
-
-# caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
-
-# uuid = config['inbounds'][0]['settings']['clients'][0]['id']
-# domain = caddy[:caddy.find(' {')]
-
 uuid="a04be4ef-1797-4ca9-a549-4385ce42494c"
 domain="www.digitalocean.com"
-# domain="denewcdn1.zizigooloovpn.com"
 domain="test.nimaxmusic.ir"
-# domain=""
 ipaddr=urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
-# ipaddr="test.nimaxmusic.ir"
+ipaddr="test.nimaxmusic.ir"
 zpath="/ws"
 zpath="/abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
 ztls="tls"
