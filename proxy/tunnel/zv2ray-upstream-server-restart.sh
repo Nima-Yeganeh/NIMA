93ab051f84c836e58507d78ff1fce3cdcc45fe7a
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-08 16:29:54 +0100

    done

---
+++ proxy/tunnel/zv2ray-upstream-server-restart.sh
@@ -0,0 +1,16 @@
+
+
+echo "Started!"
+git pull
+sudo docker rm -f $(sudo docker ps -q)
+cd utils
+sudo bash bbr.sh
+cd ..
+cd v2new/v2ray-upstream-server/
+ls -anp
+docker-compose up -d
+docker ps -a
+cd ..
+cd ..
+python3 vmess_for_upstream.py
+ls -anp
