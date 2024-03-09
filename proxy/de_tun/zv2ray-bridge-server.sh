1b4baec022f738f965ce69ba77a7225357b9a477
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-09 09:19:23 +0100

    done

--- proxy/de_tun/zv2ray-bridge-server.sh
+++ proxy/de_tun/zv2ray-bridge-server.sh
@@ -1,6 +1,34 @@
 
+
 sleeptime=0
 echo "Started!"
+while true; do
+    read -p "Enter local IP address: " local_ip
+    read -p "Enter remote IP address: " remote_ip
+    if [[ ! $local_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Error: Invalid local IP address format. Try again."
+        continue
+    fi
+    if [[ ! $remote_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Error: Invalid remote IP address format. Try again."
+        continue
+    fi
+    break
+done
+echo "Local IP: $local_ip"
+echo "Remote IP: $remote_ip"
+sudo apt update -y
+sudo ip tunnel show
+sudo ip tunnel add 6to4tun_IR mode sit remote $remote_ip local $local_ip
+sudo ip -6 addr add 2001:470:1f10:e1f::1/64 dev 6to4tun_IR
+sudo ip link set 6to4tun_IR mtu 1480
+sudo ip link set 6to4tun_IR up
+sudo ip tunnel del GRE6Tun_IR
+sudo ip -6 tunnel add GRE6Tun_IR mode ip6gre remote 2001:470:1f10:e1f::2 local 2001:470:1f10:e1f::1
+sudo ip addr add 172.16.1.1/30 dev GRE6Tun_IR
+sudo ip link set GRE6Tun_IR mtu 1436
+sudo ip link set GRE6Tun_IR up
+sudo ip tunnel show
 git pull
 sleep $sleeptime
 hostname=$(hostname)
