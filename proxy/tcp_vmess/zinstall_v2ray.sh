99931e503aa26161e8a94a1f5a1fa823116c08fb
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-10 14:54:51 +0100

    done

--- proxy/tcp_vmess/zinstall_v2ray.sh
+++ proxy/tcp_vmess/zinstall_v2ray.sh
@@ -1,13 +1,10 @@
 
 
-sleeptime=0
 echo "Started!"
-sleep $sleeptime
 sudo apt update -y
 sudo ip addr add 172.16.1.2/32 dev lo
 ip addr show lo
 sudo apt install iftop mtr -y
-sleep $sleeptime
 if [ ! -e "/swapfile" ]; then
     sudo fallocate -l 4G /swapfile
     ls -anp /swapfile
@@ -16,12 +13,10 @@ if [ ! -e "/swapfile" ]; then
     sudo swapon /swapfile
     echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
-sleep $sleeptime
 sudo apt update -y
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 python3 --version
-sleep $sleeptime
 sudo apt update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
 sudo apt-get update -y
@@ -30,10 +25,8 @@ sudo apt-get update -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker rm -f $(sudo docker ps -q)
-sleep $sleeptime
 cd utils
 sudo bash bbr.sh
-sleep $sleeptime
 cd ..
 cd v2new/v2ray-upstream-server/
 ls -anp
