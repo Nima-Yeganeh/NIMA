c49fe8bdfd77f0bfcd7c27de37567ece3c78bef3
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-12 12:28:56 +0100

    done

--- proxy/de_tun_tcp_vm_35456/vmess_for_bridge.py
+++ proxy/de_tun_tcp_vm_35456/vmess_for_bridge.py
@@ -21,7 +21,7 @@ port = "35456"
 uuid = "a04be4ef-1797-4ca9-a549-4385ce42494c"
 # security = inbound['settings']['clients'][0]['security']
 ps = "{}:{}".format(ip, port)
-ps = "@zizigooloovpn"
+ps = "@v2rayng_ip_vpn | @xyzserviceadmin"
 c = {"add": ip, "aid": "0", "host": "", "id": uuid, "net": "tcp", "path": "", "port": port, "ps": ps,
         "tls": "none", "type": "none", "v": "2"}
 j = json.dumps(c)
