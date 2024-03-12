53defb612232875591f4bdb8df3592b16fb58534
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-12 13:24:34 +0100

    done

--- proxy/de_tun_8080_xyzircdn/vmess_for_bridge.py
+++ proxy/de_tun_8080_xyzircdn/vmess_for_bridge.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from urllib.request import urlopen
 from pathlib import Path
 uuid="a04be4ef-1797-4ca9-a549-4385ce42494c"
-domain="www.digitalocean.com"
+domain="fantasticdomainnamesthatyouwilllovearethebestservicearcdn1.nimaxmusic.ir"
 ipaddr=urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
 zpath="/ws"
 zpath="/"
