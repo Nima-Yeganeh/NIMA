9b45c1017fdde58ea6168f7d9e3aaf4862587108
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-16 13:25:17 +0100

    done

--- proxy/zizicdn1_ssl/v2ray-caddy-cdn/vmess.py
+++ proxy/zizicdn1_ssl/v2ray-caddy-cdn/vmess.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 path = Path(__file__).parent
 
-config_file = open(str(path.joinpath('v2ray_upstream_config/config/config.json')), 'r', encoding='utf-8')
+config_file = open(str(path.joinpath('v2ray/config/config.json')), 'r', encoding='utf-8')
 config = json.load(config_file)
 
 caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
@@ -14,13 +14,9 @@ caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read(
 uuid = config['inbounds'][0]['settings']['clients'][0]['id']
 domain = caddy[:caddy.find(' {')]
 
-ps = "@v2rayng_ip_vpn | @xyzserviceadmin"
-addr = "www.digitalocean.com"
-
 j = json.dumps({
-    "v": "2", "ps": ps, "add": addr, "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
+    "v": "2", "ps": domain, "add": domain, "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
     "host": domain, "path": "/ws", "tls": "tls"
 })
 
 print("vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
-
