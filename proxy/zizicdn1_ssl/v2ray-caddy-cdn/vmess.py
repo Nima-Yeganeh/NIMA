ee42d9838ee0f8f467e33190d4ecf86bb1fed397
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-16 13:17:53 +0100

    done

---
+++ proxy/zizicdn1_ssl/v2ray-caddy-cdn/vmess.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import base64
+import json
+from pathlib import Path
+
+path = Path(__file__).parent
+
+config_file = open(str(path.joinpath('v2ray_upstream_config/config/config.json')), 'r', encoding='utf-8')
+config = json.load(config_file)
+
+caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
+
+uuid = config['inbounds'][0]['settings']['clients'][0]['id']
+domain = caddy[:caddy.find(' {')]
+
+ps = "@v2rayng_ip_vpn | @xyzserviceadmin"
+addr = "www.digitalocean.com"
+
+j = json.dumps({
+    "v": "2", "ps": ps, "add": addr, "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
+    "host": domain, "path": "/ws", "tls": "tls"
+})
+
+print("vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
+
