ef4996178c0764c9a25b29320ccd0fac388af751
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-17 10:26:00 +0100

    done

--- proxy/tcp_vmess_http/zrestart_v2ray.sh
+++ proxy/tcp_vmess_http/zrestart_v2ray.sh
@@ -1,7 +1,7 @@
 
 git pull
+sudo docker rm -f v2ray-upstream-server_v2ray_1 v2ray-bridge-server_v2ray_1
 cd v2new/v2ray-upstream-server/
-ls -anp
 docker-compose up -d
 docker ps -a
 cd ..
@@ -12,4 +12,4 @@ docker ps -a
 cd ..
 cd ..
 python3 vmess_for_bridge.py
-ls -anp
+
