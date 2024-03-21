408b95ef8d6ea7cdd88fe028402167e2f89bde46
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-03-21 13:36:30 +0100

    done

--- proxy/cdninstall.sh
+++ proxy/cdninstall.sh
@@ -1,10 +1,5 @@
 
-sleeptime=0
 echo "Started!"
-sleep $sleeptime
-sudo apt update -y
-sudo apt install screen iftop mtr -y
-sleep $sleeptime
 if [ ! -e "/swapfile" ]; then
     sudo fallocate -l 4G /swapfile
     ls -anp /swapfile
@@ -13,30 +8,14 @@ if [ ! -e "/swapfile" ]; then
     sudo swapon /swapfile
     echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
-sleep $sleeptime
 sudo apt update -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-python3 --version
-sleep $sleeptime
-sudo apt update -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
-if [ ! -e "/usr/share/keyrings/docker-archive-keyring.gpg" ]; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-fi
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo apt install docker-compose -y
+sudo apt install screen iftop mtr python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo docker rm -f $(sudo docker ps -q)
-sleep $sleeptime
 cd utils
 sudo bash bbr.sh
-sleep $sleeptime
 cd ..
 sudo docker compose up -d
- sudo docker ps -a
+sudo docker ps -a
 python3 vmess.py
 echo "Done!"
