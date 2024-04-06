32ab05b580c658ccca9275ce9dddace5081fb0d1
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-06 18:44:56 +0100

    done

--- proxy/vmess_tun_tcp2/upstream.sh
+++ proxy/vmess_tun_tcp2/upstream.sh
@@ -27,10 +27,10 @@ sudo ip tunnel show
 sudo ip tunnel del 6to4tun_IR12
 sudo ip tunnel del GRE6Tun_IR12
 sudo ip tunnel add 6to4tun_IR12 mode sit remote $remote_ip local $local_ip
-sudo ip -6 addr add 2001:2001:2001:12:2/64 dev 6to4tun_IR12
+sudo ip -6 addr add 2001:2001:2001:12::2/64 dev 6to4tun_IR12
 sudo ip link set 6to4tun_IR12 mtu 1480
 sudo ip link set 6to4tun_IR12 up
-sudo ip -6 tunnel add GRE6Tun_IR12 mode ip6gre remote 2001:2001:2001:12:1 local 2001:2001:2001:12:2
+sudo ip -6 tunnel add GRE6Tun_IR12 mode ip6gre remote 2001:2001:2001:12::1 local 2001:2001:2001:12::2
 sudo ip addr add 172.16.12.2/30 dev GRE6Tun_IR12
 sudo ip link set GRE6Tun_IR12 mtu 1436
 sudo ip link set GRE6Tun_IR12 up
