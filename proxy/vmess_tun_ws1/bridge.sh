4ec0a9681bad9d63d97e6b5aa41058783e0a4bcc
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-08 12:59:24 +0100

    done

--- proxy/vmess_tun_ws1/bridge.sh
+++ proxy/vmess_tun_ws1/bridge.sh
@@ -1,20 +1,5 @@
 
 echo "Started!"
-while true; do
-    read -p "Enter local IP address: " local_ip
-    read -p "Enter remote IP address: " remote_ip
-    if [[ ! $local_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Error: Invalid local IP address format. Try again."
-        continue
-    fi
-    if [[ ! $remote_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Error: Invalid remote IP address format. Try again."
-        continue
-    fi
-    break
-done
-echo "Local IP: $local_ip"
-echo "Remote IP: $remote_ip"
 sudo ip tunnel show
 sudo ip tunnel del 6to4tun_IR21
 sudo ip tunnel del GRE6Tun_IR21
