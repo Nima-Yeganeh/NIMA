de8322c69116c12a59e735cab1f6c35db0c07403
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-08 10:37:36 +0100

    done

--- proxy/vmess_tun_ws1/vmess_for_bridge.py
+++ proxy/vmess_tun_ws1/vmess_for_bridge.py
@@ -10,21 +10,15 @@ from pathlib import Path
 
 uuid="a04be4ef-1797-4ca9-a549-4385ce42494c"
 ipaddr=urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
-
+ip = urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
+domain=""
 zpath="/"
 ztls=""
-zport="80"
+zport="35021"
+ps = "SRV21 @v2rayng_ip_vpn | @zizigooloovpn << Telegram"
 
-ip = urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
-
-port = "35012"
-uuid = "a04be4ef-1797-4ca9-a549-4385ce42494c"
-
-ps = "{}:{}".format(ip, port)
-ps = "SRV12 @v2rayng_ip_vpn | @zizigooloovpn << Telegram"
-
-c = {"add": ip, "aid": "0", "host": "", "id": uuid, "net": "tcp", "path": "", "port": port, "ps": ps,
-        "tls": "none", "type": "none", "v": "2"}
-j = json.dumps(c)
-link = "vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii')
-print("\nVMESS:\n" + link)
+j = json.dumps({
+    "v": "2", "ps": "@v2rayng_ip_vpn | @xyzserviceadmin", "add": ipaddr, "port": zport, "id": uuid, "aid": "0", "net": "ws", "type": "none",
+    "host": domain, "path": zpath, "tls": ztls
+})
+print("vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
