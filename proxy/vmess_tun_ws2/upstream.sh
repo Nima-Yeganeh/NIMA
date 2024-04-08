223572b330ddd9469ff7ee15f5ec7be6276bb3ad
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-08 18:37:30 +0100

    done

--- proxy/vmess_tun_ws2/upstream.sh
+++ proxy/vmess_tun_ws2/upstream.sh
@@ -8,8 +8,8 @@ if [ ! -e "/swapfile" ]; then
     sudo swapon /swapfile
     echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
-sudo ip tunnel del 6to4tun_IR21
-sudo ip tunnel del GRE6Tun_IR21
+sudo ip tunnel del 6to4tun_IR22
+sudo ip tunnel del GRE6Tun_IR22
 sudo ip tunnel show
 sudo apt update -y
 sudo apt install iftop mtr python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release docker-compose -y
