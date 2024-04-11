cb8f970a4ff4ffb8770a90bb790aae6e12a5d566
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-11 13:55:18 +0100

    done

---
+++ proxy/vmess_direct_notun_br_up_tcp_ws2/vmess_for_bridge2.py
@@ -0,0 +1,24 @@
+
+import base64
+import json
+import base64
+import json
+import re
+from pathlib import Path
+from urllib.request import urlopen
+from pathlib import Path
+
+uuid="a04be4ef-1797-4ca9-a549-4385ce42494c"
+ipaddr=urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
+ip = urlopen("https://ipv4.icanhazip.com/").read().decode().rstrip()
+domain=""
+zpath="/"
+ztls=""
+zport="35022"
+ps = "SRV22 @v2rayng_ip_vpn | @zizigooloovpn << Telegram"
+
+j = json.dumps({
+    "v": "2", "ps": ps, "add": ipaddr, "port": zport, "id": uuid, "aid": "0", "net": "ws", "type": "none",
+    "host": domain, "path": zpath, "tls": ztls
+})
+print("vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
