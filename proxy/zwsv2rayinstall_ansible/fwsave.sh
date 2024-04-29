622658fdb5cb93d8be4fd7070dae7176b2230971
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-04-29 18:37:57 +0200

    done

---
+++ proxy/zwsv2rayinstall_ansible/fwsave.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Save iptables rules
+sudo iptables-save > /etc/iptables/rules.v4
---
+++ proxy/zwsv2rayinstall_ansible/rebootnow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true; do
+    # Get the current hour and minute
+    current_hour=$(date +"%H")
+    current_minute=$(date +"%M")
+    # Check if the current time is after 4:00 AM and before 4:01 AM
+    if [ $current_hour -eq 4 ] && [ $current_minute -eq 0 ]; then
+        reboot now
+    fi
+    # Wait for 30 seconds before checking again
+    sleep 30
+done
+
