77c7887ec2ce45d3c9569bf0487efd7bde4604c2
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-03 14:37:48 +0200

    done

--- proxy/ansibletunprnewv2/configupdate.sh
+++ proxy/ansibletunprnewv2/configupdate.sh
@@ -47,16 +47,16 @@ echo_server_combinations() {
     # echo "$srvir_ip $srvir_name $srvir_num $srvdigi_ip $srvdigi_name $srvdigi_num" >> $srvdigitunconfigfilename
 
     echo "" > $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35011 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35012 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35013 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35014 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35015 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35021 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35022 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35023 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35024 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
-    echo "sudo iptables -t nat -D PREROUTING -p tcp --dport 35025 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35011 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35012 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35013 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35014 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35015 -j DNAT --to-destination $srvir_ip:8001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35021 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35022 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35023 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35024 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
+    echo "sudo iptables -t nat -A PREROUTING -p tcp --dport 35025 -j DNAT --to-destination $srvir_ip:7001" >> $srvirzallnatconfigfilename
 
     echo "sudo ip tunnel del $irtun" >> $srvirtundelconfigfilename
     echo "sudo ip -6 tunnel del $irtungre" >> $srvirtundelconfigfilename
