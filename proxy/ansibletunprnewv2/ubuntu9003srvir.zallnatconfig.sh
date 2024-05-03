d7134b71f5bf82eae7dfc9f8eac7d171eda566ba
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-03 12:43:28 +0000

    done

--- proxy/ansibletunprnewv2/ubuntu9003srvir.zallnatconfig.sh
+++ proxy/ansibletunprnewv2/ubuntu9003srvir.zallnatconfig.sh
@@ -1,11 +1,11 @@
 
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35011 -j DNAT --to-destination 94.182.223.249:8001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35012 -j DNAT --to-destination 94.182.223.249:8001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35013 -j DNAT --to-destination 94.182.223.249:8001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35014 -j DNAT --to-destination 94.182.223.249:8001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35015 -j DNAT --to-destination 94.182.223.249:8001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35021 -j DNAT --to-destination 94.182.223.249:7001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35022 -j DNAT --to-destination 94.182.223.249:7001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35023 -j DNAT --to-destination 94.182.223.249:7001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35024 -j DNAT --to-destination 94.182.223.249:7001
-sudo iptables -t nat -D PREROUTING -p tcp --dport 35025 -j DNAT --to-destination 94.182.223.249:7001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35011 -j DNAT --to-destination 94.182.223.249:8001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35012 -j DNAT --to-destination 94.182.223.249:8001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35013 -j DNAT --to-destination 94.182.223.249:8001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35014 -j DNAT --to-destination 94.182.223.249:8001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35015 -j DNAT --to-destination 94.182.223.249:8001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35021 -j DNAT --to-destination 94.182.223.249:7001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35022 -j DNAT --to-destination 94.182.223.249:7001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35023 -j DNAT --to-destination 94.182.223.249:7001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35024 -j DNAT --to-destination 94.182.223.249:7001
+sudo iptables -t nat -A PREROUTING -p tcp --dport 35025 -j DNAT --to-destination 94.182.223.249:7001
