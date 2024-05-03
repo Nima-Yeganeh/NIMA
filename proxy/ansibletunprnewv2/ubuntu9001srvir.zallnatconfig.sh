c72ab67ff3f6e167a64750ffac783e03050af46b
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-03 12:37:57 +0000

    done

---
+++ proxy/ansibletunprnewv2/ubuntu9001srvir.zallnatconfig.sh
@@ -0,0 +1,11 @@
+
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35011 -j DNAT --to-destination 31.216.62.115:8001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35012 -j DNAT --to-destination 31.216.62.115:8001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35013 -j DNAT --to-destination 31.216.62.115:8001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35014 -j DNAT --to-destination 31.216.62.115:8001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35015 -j DNAT --to-destination 31.216.62.115:8001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35021 -j DNAT --to-destination 31.216.62.115:7001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35022 -j DNAT --to-destination 31.216.62.115:7001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35023 -j DNAT --to-destination 31.216.62.115:7001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35024 -j DNAT --to-destination 31.216.62.115:7001
+sudo iptables -t nat -D PREROUTING -p tcp --dport 35025 -j DNAT --to-destination 31.216.62.115:7001
