e4afeb234ab45681a51f9e140d573c340c1fd291
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-12 16:21:34 +0200

    done

--- proxy/zmine_mt_plus_tcp_vmess_http_config/zmt_docker_install.sh
+++ proxy/zmine_mt_plus_tcp_vmess_http_config/zmt_docker_install.sh
@@ -1,4 +1,12 @@
 sudo lsb_release -a
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
 sudo apt update -y
 sudo apt install iftop mtr screen net-tools docker-compose -y
 sudo systemctl start docker
