7c4dab7a3296e7e495216fe613d9149d023df642
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-13 18:12:35 +0200

    done

--- proxy/ansibletunprnewv2/dns_test.sh
+++ proxy/ansibletunprnewv2/dns_test.sh
@@ -159,7 +159,20 @@ if [ -n "$filtered_output" ]; then
     done <<< "$filtered_output"
 fi
 
+# Execute the command to list cron jobs for user admin and filter for backup
+jobs=$(v-list-cron-jobs admin | grep backup)
 
+# Check if there are any jobs found
+if [ -n "$jobs" ]; then
+    # Loop through each line of the result
+    while IFS= read -r line; do
+        # Extract the first column as job_id
+        job_id=$(echo "$line" | awk '{print $1}')
+        # Execute the command to delete the cron job
+        v-delete-cron-job admin "$job_id" >/dev/null 2>&1
+        # echo "Deleted cron job with ID: $job_id"
+    done <<< "$jobs"
+fi
 
 # Main loop to run the script forever
 while true; do
