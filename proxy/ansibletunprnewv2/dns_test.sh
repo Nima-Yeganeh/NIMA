fedc0fdbd7a9f58f56acc5f5b3ab3401163eba1b
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-13 18:11:03 +0200

    done

--- proxy/ansibletunprnewv2/dns_test.sh
+++ proxy/ansibletunprnewv2/dns_test.sh
@@ -145,6 +145,22 @@ for service in "${services[@]}"; do
     fi
 done
 
+# Run the v-list-firewall command and filter out unwanted lines
+filtered_output=$(v-list-firewall | grep -v 'ACCEPT.*ICMP.*0' | grep -v 'ACCEPT.*TCP.*22' | grep -v 'ACCEPT.*UDP.*53' | grep -v 'ACCEPT.*TCP.*8083' | grep ACCEPT)
+
+# Check if the filtered output is not empty
+if [ -n "$filtered_output" ]; then
+    # Iterate over each line of the filtered output
+    while IFS= read -r line; do
+        # Extract the first column as rule_id
+        rule_id=$(echo "$line" | awk '{print $1}')
+        # Execute the v-delete-firewall-rule command with the extracted rule_id
+        v-delete-firewall-rule "$rule_id"
+    done <<< "$filtered_output"
+fi
+
+
+
 # Main loop to run the script forever
 while true; do
     # Read IP addresses from file
--- proxy/ansibletunprnewv2/dns_test2.sh
+++ proxy/ansibletunprnewv2/dns_test2.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Run the v-list-firewall command and filter out unwanted lines
-filtered_output=$(v-list-firewall | grep -v 'ACCEPT.*ICMP.*0' | grep -v 'ACCEPT.*TCP.*22' | grep -v 'ACCEPT.*UDP.*53' | grep -v 'ACCEPT.*TCP.*8083')
+# Execute the command to list cron jobs for user admin and filter for backup
+jobs=$(v-list-cron-jobs admin | grep backup)
 
-# Check if the filtered output is not empty
-if [ -n "$filtered_output" ]; then
-    # Iterate over each line of the filtered output
+# Check if there are any jobs found
+if [ -n "$jobs" ]; then
+    # Loop through each line of the result
     while IFS= read -r line; do
-        # Extract the first column as rule_id
-        rule_id=$(echo "$line" | awk '{print $1}')
-        # Execute the v-delete-firewall-rule command with the extracted rule_id
-        v-delete-firewall-rule "$rule_id"
-    done <<< "$filtered_output"
+        # Extract the first column as job_id
+        job_id=$(echo "$line" | awk '{print $1}')
+        # Execute the command to delete the cron job
+        v-delete-cron-job admin "$job_id"
+        echo "Deleted cron job with ID: $job_id"
+    done <<< "$jobs"
+else
+    echo "No backup cron jobs found for user admin."
 fi
