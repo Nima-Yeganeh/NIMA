e2ff5c950ac61c20392ae241ac0ee915b59d188a
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-13 19:35:16 +0200

    done

---
+++ proxy/ansibletunprnewv2/dns_install_script.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Prompt user for password
+read -p "Enter your password: " PASSWORD
+echo
+
+# Check if password is not empty
+if [ -z "$PASSWORD" ]; then
+    echo "Password cannot be empty. Exiting."
+    exit 1
+fi
+
+# Ask user if they want to continue
+read -p "Do you want to continue? (y/yes): " CONTINUE
+
+# Check if user wants to continue
+if [[ "$CONTINUE" != "y" && "$CONTINUE" != "yes" ]]; then
+    echo "Exiting."
+    exit 0
+fi
+
+# Specify the size of the swap file in megabytes
+SWAP_SIZE_MB=2048
+
+# Check if /swapfile already exists
+if [ ! -f "/swapfile" ]; then
+    # Create a swap file
+    sudo fallocate -l ${SWAP_SIZE_MB}M /swapfile
+
+    # Set appropriate permissions for the swap file
+    sudo chmod 600 /swapfile
+
+    # Set up the swap space
+    sudo mkswap /swapfile
+
+    # Enable the swap file
+    sudo swapon /swapfile
+
+    # Add the swap file to /etc/fstab to make the change persistent across reboots
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+    # Check if swap has been activated
+    sudo swapon --show
+
+    # Check the swap usage
+    sudo free -h
+else
+    echo "Swap file already exists at /swapfile."
+fi
+
+sleep 30
+
+sudo yum update -y
+sudo yum install nc curl -y
+
+sleep 30
+
+curl -O /vst-install.sh http://vestacp.com/pub/vst-install.sh
+
+cp dns_test.sh /dns_test.sh
+
+# Copy dns_updatescript.service to services directory
+sudo cp dns_updatescript.service /etc/systemd/system/
+
+# Reload systemd daemon to read the new service file
+sudo systemctl daemon-reload
+
+# Enable the service
+sudo systemctl enable dns_updatescript.service
+
+# Execute vst-install.sh script with provided options
+bash /vst-install.sh --email info@domain.local \
+                    --password "$PASSWORD" \
+                    --apache no \
+                    --phpfpm no \
+                    --nginx no \
+                    --vsftpd no \
+                    --proftpd no \
+                    --named yes \
+                    --mysql no \
+                    --postgresql no \
+                    --exim no \
+                    --dovecot no \
+                    --clamav no \
+                    --spamassassin no \
+                    --iptables yes \
+                    --fail2ban no \
+                    --remi no \
+                    --softaculous no \
+                    --quota no \
+                    --hostname ubuntu \
+                    --interactive no \
+                    --force
+
+# Start the service
+sudo systemctl start dns_updatescript.service
+
+# Check the status of the service
+# sudo systemctl status dns_updatescript.service
+
