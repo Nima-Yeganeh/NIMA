4a3bc0ab862a61c519fd7cda8514fb18367450e0
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-14 17:02:48 +0200

    done

--- proxy/ansibletunprnewv2/vps_install_script.sh
+++ proxy/ansibletunprnewv2/vps_install_script.sh
@@ -1,7 +1,6 @@
-# sudo apt update -y
-sudo apt update -y > /dev/null 2>&1
+#!/bin/bash
 
-# sudo apt install docker-compose iftop iotop mtr htop mtr screen traceroute iptables-persistent net-tools socat python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release -y
+sudo apt update -y > /dev/null 2>&1
 
 sudo apt install docker-compose iftop iotop mtr htop mtr screen traceroute iptables-persistent net-tools socat python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release -y >/dev/null 2>&1
 
@@ -13,25 +12,26 @@ echo "export HISTSIZE=0" | sudo tee -a /etc/bash.bashrc > /dev/null
 
 sudo timedatectl set-timezone Asia/Tehran
 
-cp -f fwsave.sh /fwsave.sh
+cp -f /nima/proxy/ansibletunprnewv2/fwsave.sh /fwsave.sh
 
 sudo bash /fwsave.sh
 
 public_ip=$(curl -s ifconfig.me)
-echo "Public IP address: $public_ip"
+# echo "Public IP address: $public_ip"
 
 ipv4_address=$(ifconfig enp1s0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 if [ -z "$ipv4_address" ]; then
     ipv4_address=$(ip -o -4 addr show enp1s0 | awk '{print $4}' | cut -d'/' -f1)
 fi
-echo "IPv4 address of enp1s0: $ipv4_address"
+# echo "IPv4 address of enp1s0: $ipv4_address"
 
-cp -f vps_v2ray_up.conf /vps_v2ray_up.conf
-cp -f vps_v2ray_br.conf /vps_v2ray_br.conf
-cp -f vps_docker_compose.yml /docker-compose.yml
+cp -f /nima/proxy/ansibletunprnewv2/vps_v2ray_up.conf /vps_v2ray_up.conf
+cp -f /nima/proxy/ansibletunprnewv2/vps_v2ray_br.conf /vps_v2ray_br.conf
+cp -f /nima/proxy/ansibletunprnewv2/vps_docker_compose.yml /docker-compose.yml
 sed -i "s/PUBLICIPADDR/$ipv4_address/g" /vps_v2ray_br.conf
 
 cd /
 pwd
 sudo docker-compose up -d
-sudo docker ps -a
+# sudo docker ps -a
+
---
+++ proxy/ansibletunprnewv2/vps_test.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Install sshpass silently
+sudo apt install sshpass -y >/dev/null 2>&1
+
+# Function to check if destination IP and port are open
+check_port_open() {
+    nc -zvw1 $1 $2 2>/dev/null && return 0 || return 1
+}
+
+# Function to add IP and location to vps_ips.txt
+add_ip_location() {
+    echo "$1,$2" >> vps_ips.txt
+}
+
+# Function to copy SSH public key to remote server
+copy_ssh_key() {
+    sshpass -p "$PASSWORD" ssh-copy-id -o "StrictHostKeyChecking=no" -p $PORT $USER@$IP
+}
+
+# Function to execute commands on remote server via SSH
+execute_commands() {
+    sshpass -p "$PASSWORD" ssh -o "StrictHostKeyChecking=no" -p $PORT $USER@$IP "$1"
+}
+
+# Default values
+DEFAULT_PORT="22"
+DEFAULT_LOCATION="nl"
+DEFAULT_USER="root"
+
+# Prompt user for input
+read -p "Enter IP address: " IP
+# Validate IP address format
+if [[ ! $IP =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    echo "Invalid IP address format. Please enter a valid IPv4 address."
+    exit 1
+fi
+
+read -p "Enter port [Default: $DEFAULT_PORT]: " PORT
+PORT=${PORT:-$DEFAULT_PORT}
+read -p "Enter location [Default: $DEFAULT_LOCATION]: " LOCATION
+LOCATION=${LOCATION:-$DEFAULT_LOCATION}
+read -p "Enter username [Default: $DEFAULT_USER]: " USER
+USER=${USER:-$DEFAULT_USER}
+read -p "Enter password: " PASSWORD
+echo
+
+# Check if destination IP and port are open
+if check_port_open $IP $PORT; then
+    echo "Destination IP and port are open."
+
+    # Copy SSH public key to remote server
+    copy_ssh_key
+    echo "SSH public key copied to $IP successfully."
+
+    # Prompt user for new server password
+    read -p "Enter new password for server: " NEW_PASSWORD
+    echo
+
+    # Execute commands to update passwords on remote server
+    execute_commands "echo 'linuxuser:$NEW_PASSWORD' | sudo chpasswd"
+    execute_commands "echo 'root:$NEW_PASSWORD' | sudo chpasswd"
+    execute_commands "echo 'ubuntu:$NEW_PASSWORD' | sudo chpasswd"
+    execute_commands "sed -i 's/^Port $PORT$/Port 22/' /etc/ssh/sshd_config"
+    execute_commands "sudo systemctl restart sshd"
+    execute_commands "sudo systemctl restart ssh"
+    execute_commands "sudo apt install git -y >/dev/null 2>&1"
+    execute_commands "sudo rm -rf /nima >/dev/null 2>&1"
+    execute_commands "git clone https://github.com/nima-yeganeh/nima /nima"
+    execute_commands "sudo bash /nima/proxy/ansibletunprnewv2/vps_install_script.sh"
+
+    echo "Passwords updated on $IP."
+else
+    echo "Destination IP and port are not open."
+fi
+
+# Check if IP address is already in vps_ips.txt
+if ! grep -q "^$IP," vps_ips.txt; then
+    # Add IP and location to vps_ips.txt
+    add_ip_location "$IP" "$LOCATION"
+fi
+
+# Output collected information
+echo "IP: $IP"
+echo "Port: $PORT"
+echo "Location: $LOCATION"
+echo "User: $USER"
+echo "Done!"
+
