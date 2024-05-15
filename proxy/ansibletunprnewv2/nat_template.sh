e323732f47ffff5a152503942e303d4d5d4b3ff1
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-15 10:57:13 +0200

    done

--- proxy/ansibletunprnewv2/nat_template.sh
+++ proxy/ansibletunprnewv2/nat_template.sh
@@ -10,4 +10,8 @@
 [ $(sudo iptables -t nat -L | grep '35024' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 35024 -j DNAT --to-destination PUBLICIPADDR:7001
 [ $(sudo iptables -t nat -L | grep '35025' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 35025 -j DNAT --to-destination PUBLICIPADDR:7001
 [ $(sudo iptables -t nat -L | grep '8080' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination PUBLICIPADDR:7001
-[ $(sudo iptables -t nat -L | grep '8443' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 8443 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep '8880' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 8880 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep '2052' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2052 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep '2082' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2082 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep '2086' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2086 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep '2095' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2095 -j DNAT --to-destination PUBLICIPADDR:7001
