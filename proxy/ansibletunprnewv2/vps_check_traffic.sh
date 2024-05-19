f540f9e88b7fe950334cf37eea1e55be2efadbb2
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-19 11:52:47 +0000

    done

---
+++ proxy/ansibletunprnewv2/vps_check_traffic.sh
@@ -0,0 +1,2 @@
+salt "*" cmd.run "sudo bash /nima/proxy/ansibletunprnewv2/check_traffic.sh" | grep Traffic
+
---
+++ proxy/ansibletunprnewv2/vps_traffic_rate.sh
@@ -0,0 +1,2 @@
+salt "*" cmd.run "sudo bash /nima/proxy/ansibletunprnewv2/traffic_rate.sh" | grep Traffic
+
