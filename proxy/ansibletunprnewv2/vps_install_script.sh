b994c8c4b3c5c79ed545aa7c4a3d23d969aa0a6e
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-19 09:28:41 +0000

    done

--- proxy/ansibletunprnewv2/vps_install_script.sh
+++ proxy/ansibletunprnewv2/vps_install_script.sh
@@ -156,7 +156,7 @@ sudo systemctl start "$SERVICE_FILE" > /dev/null 2>&1
 
 curl -L https://bootstrap.saltstack.com -o /install_salt.sh > /dev/null 2>&1
 sudo bash /install_salt.sh > /dev/null 2>&1
-
-sudo systemctl status salt-minion
+result="master: $(head -n 1 /nima/proxy/ansibletunprnewv2/saltmaster.txt)"; grep -qxF "$result" /etc/salt/minion || echo "$result" | sudo tee -a /etc/salt/minion
+sudo systemctl restart salt-minion > /dev/null 2>&1
 
 
