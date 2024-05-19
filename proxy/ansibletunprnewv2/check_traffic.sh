214d010d1077af99f8913e48c0e721c22fb97d0f
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-19 10:22:26 +0000

    done

--- proxy/ansibletunprnewv2/check_traffic.sh
+++ proxy/ansibletunprnewv2/check_traffic.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Interface name
-interface="eth0"
+interface=$({ ip link show eth0 &>/dev/null && echo "eth0"; } || { ip link show enp1s0 &>/dev/null && echo "enp1s0"; })
 
 # Extract send and receive statistics
 send=$(ifconfig $interface | grep "TX packets" | awk '{print $6 " " $7}')
 receive=$(ifconfig $interface | grep "RX packets" | awk '{print $6 " " $7}')
 
 # Display statistics
-echo "Send Traffic: $send packets and Receive Traffic: $receive packets"
+echo "$(hostname) >> IP:$(head -n1 /serverip.txt 2>/dev/null) >> Send Traffic: $send packets and Receive Traffic: $receive packets"
 
 # interface="eth0"; send=$(ifconfig $interface | grep "TX packets" | awk '{print $6 " " $7}'); receive=$(ifconfig $interface | grep "RX packets" | awk '{print $6 " " $7}'); echo "Send Traffic: $send packets and Receive Traffic: $receive packets"
 
