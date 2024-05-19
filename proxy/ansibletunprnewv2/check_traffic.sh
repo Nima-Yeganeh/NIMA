af171f8475e91667185b9194dcd38a9e04123a5c
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-19 12:24:08 +0000

    done

--- proxy/ansibletunprnewv2/check_traffic.sh
+++ proxy/ansibletunprnewv2/check_traffic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Interface name
-interface=$({ ip link show eth0 &>/dev/null && echo "eth0"; } || { ip link show enp1s0 &>/dev/null && echo "enp1s0"; } || { ip link show ens3 &>/dev/null && echo "ens3"; })
+interface=$({ ip link show eth0 &>/dev/null && echo "eth0"; } || { ip link show enp1s0 &>/dev/null && echo "enp1s0"; } || { ip link show ens3 &>/dev/null && echo "ens3"; } || { ip link show enp3s0 &>/dev/null && echo "enp3s0"; })
 
 # Extract send and receive statistics
 send=$(ifconfig $interface | grep "TX packets" | awk '{print $6 " " $7}')
--- proxy/ansibletunprnewv2/vps_install_script.sh
+++ proxy/ansibletunprnewv2/vps_install_script.sh
@@ -66,6 +66,7 @@ public_ip=$(curl -s ifconfig.me)
 ipv4_address=$(ifconfig enp1s0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 [ -z "$ipv4_address" ] && ipv4_address=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 [ -z "$ipv4_address" ] && ipv4_address=$(ifconfig ens3 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
+[ -z "$ipv4_address" ] && ipv4_address=$(ifconfig enp3s0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 echo $ipv4_address
 
 sudo cp -f $ZGITPATH/bbr.sh /bbr.sh
