38169c3cdafe1e2d9ca855f71b8ef9199a235db7
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-21 22:25:58 +0200

    done

--- proxy/ansibletunprnewv2/update_user_nat_config_v2.sh
+++ proxy/ansibletunprnewv2/update_user_nat_config_v2.sh
@@ -6,6 +6,8 @@ users_nat_deleted="/users_nat_deleted.txt"
 users_nat_created="/users_nat_created.txt"
 tempfile1="/tempfile1.txt"
 tempfile2="/tempfile2.txt"
+sudo bash /fwsave.sh
+echo "Started..."
 while true; do
     [ ! -f "$users_nat_deleted" ] && touch "$users_nat_deleted"
     [ ! -f "$users_nat_created" ] && touch "$users_nat_created"
@@ -19,22 +21,26 @@ while true; do
         expire_date="${fields[6]}"
         natport="${fields[4]}"
         dnatport="${fields[7]}"
-        count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+        # count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+        count=$(sudo cat /etc/iptables/rules.v4 | grep $natport | wc -l)
         if [ "$count" -gt 0 ]; then
             echo "Action >> Delete" > /dev/null 2>&1
             sudo iptables -t nat -D PREROUTING -p tcp --dport $natport -j DNAT --to-destination $serverip:$dnatport > /dev/null 2>&1
         fi
+        sudo bash /fwsave.sh
         echo "$(IFS=','; echo "${fields[*]}")" >> $users_nat_deleted
     done < $tempfile1
     while IFS=',' read -r -a fields; do
         expire_date="${fields[6]}"
         natport="${fields[4]}"
         dnatport="${fields[7]}"
-        count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+        # count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+        count=$(sudo cat /etc/iptables/rules.v4 | grep $natport | wc -l)
         if [ "$count" -eq 0 ]; then
             echo "Action >> Create" > /dev/null 2>&1
             sudo iptables -t nat -A PREROUTING -p tcp --dport $natport -j DNAT --to-destination $serverip:$dnatport > /dev/null 2>&1
         fi
+        sudo bash /fwsave.sh
         echo "$(IFS=','; echo "${fields[*]}")" >> $users_nat_created
     done < $tempfile2
     rm -f $tempfile1
