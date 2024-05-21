85cc1f871e71c237a761168c2cbc76fbad1ed69a
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-21 21:13:47 +0200

    done

---
+++ proxy/ansibletunprnewv2/update_user_nat_config_v2.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+ztimersleep=120
+userdb="/userdb.txt"
+serveripfile="/serverip.txt"
+users_nat_deleted="/users_nat_deleted.txt"
+users_nat_created="/users_nat_created.txt"
+tempfile1="/tempfile1.txt"
+tempfile2="/tempfile2.txt"
+while true; do
+    [ ! -f "$users_nat_deleted" ] && touch "$users_nat_deleted"
+    [ ! -f "$users_nat_created" ] && touch "$users_nat_created"
+    grep -vFf $users_nat_deleted $userdb > $tempfile1
+    wc -l $tempfile1
+    grep -vFf $users_nat_created $userdb > $tempfile2
+    wc -l $tempfile2
+    today=$(date +%Y-%m-%d)
+    serverip=$(cat $serveripfile | head -n 1)
+    while IFS=',' read -r -a fields; do
+        expire_date="${fields[6]}"
+        if [[ "$expire_date" < "$today" ]]; then
+            natport="${fields[4]}"
+            dnatport="${fields[7]}"
+            count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+            if [ "$count" -gt 0 ]; then
+                echo "Action >> Delete" > /dev/null 2>&1
+                sudo iptables -t nat -D PREROUTING -p tcp --dport $natport -j DNAT --to-destination $serverip:$dnatport > /dev/null 2>&1
+            fi
+            echo "$(IFS=','; echo "${fields[*]}")" >> $users_nat_deleted
+        else
+            echo "Action >> Create" > /dev/null 2>&1
+        fi
+    done < $tempfile1
+    while IFS=',' read -r -a fields; do
+        expire_date="${fields[6]}"
+        if [[ "$expire_date" < "$today" ]]; then
+            echo "Action >> Delete" > /dev/null 2>&1
+        else
+            natport="${fields[4]}"
+            dnatport="${fields[7]}"
+            count=$(sudo iptables -t nat -L | grep $natport | wc -l)
+            if [ "$count" -eq 0 ]; then
+                echo "Action >> Create" > /dev/null 2>&1
+                sudo iptables -t nat -A PREROUTING -p tcp --dport $natport -j DNAT --to-destination $serverip:$dnatport > /dev/null 2>&1
+            fi
+            echo "$(IFS=','; echo "${fields[*]}")" >> $users_nat_created
+        fi
+    done < $tempfile2
+    rm -f $tempfile1
+    rm -f $tempfile2
+    sudo bash /fwsave.sh
+    sleep $ztimersleep
+done
