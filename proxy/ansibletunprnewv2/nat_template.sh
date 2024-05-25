b4f9034056750b9c84d17b605d4cd62f1fb4f751
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-25 07:45:08 +0000

    done

--- proxy/ansibletunprnewv2/nat_template.sh
+++ proxy/ansibletunprnewv2/nat_template.sh
@@ -15,4 +15,5 @@
 [ $(sudo iptables -t nat -L | grep 'dpt:2086 ' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2086 -j DNAT --to-destination PUBLICIPADDR:7001
 [ $(sudo iptables -t nat -L | grep 'dpt:2095 ' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 2095 -j DNAT --to-destination PUBLICIPADDR:7001
 [ $(sudo iptables -t nat -L | grep 'dpt:80 ' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination PUBLICIPADDR:7001
+[ $(sudo iptables -t nat -L | grep 'dpt:443 ' | wc -l) -eq 0 ] && sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination PUBLICIPADDR:7001
 
