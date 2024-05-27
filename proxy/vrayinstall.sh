b46b3e676a58244a528343c713edb1cc11d90f75
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-05-27 07:04:20 +0000

    done

--- proxy/vrayinstall.sh
+++ proxy/vrayinstall.sh
@@ -10,7 +10,7 @@ export LANG=en_US.UTF-8
 
 zzzport="443"
 realdomainname="nimaxmusic.ir"
-domain_hostname="vls4de"
+domain_hostname="vls5az"
 zzzdomain="$domain_hostname.$realdomainname"
 
 echo $domain_hostname
