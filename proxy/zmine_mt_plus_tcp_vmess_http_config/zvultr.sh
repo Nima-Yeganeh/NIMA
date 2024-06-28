138d7816f18ca98f5cd18250f47a18075974afa1
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-28 11:48:36 +0200

    done

---
+++ proxy/zmine_mt_plus_tcp_vmess_http_config/zvultr.sh
@@ -0,0 +1,31 @@
+swapoff -a
+rm -f /swapfile
+sudo lsb_release -a
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
+sudo apt update -y
+sudo apt install iftop mtr screen net-tools docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker ps -a
+sudo docker run --cap-add=NET_ADMIN --device=/dev/net/tun -d --name mikrotik -p 5443:443 --restart always -ti nimaxnimax/dockermtnimaxsectestlab
+sudo docker ps -a
+sudo systemctl start docker
+sudo systemctl enable docker
+cd utils
+sudo bash bbr.sh
+cd ..
+git pull
+sudo docker rm -f v2ray-upstream-server_v2ray_1 v2ray-bridge-server_v2ray_1
+cd v2new/v2ray-upstream-server/
+docker-compose up -d
+cd ..
+cd ..
+docker ps -a
+
