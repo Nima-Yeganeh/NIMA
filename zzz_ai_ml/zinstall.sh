00364ba0a487e09bd907498bac789e24a5a9350f
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-07-19 10:07:57 +0000

    done

--- zzz_ai_ml/zinstall.sh
+++ zzz_ai_ml/zinstall.sh
@@ -1,6 +1,11 @@
 sudo apt update -y
 sudo apt install python3 python3-pip -y
 sudo apt install python-is-python3
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev -y
 python --version
 pip --version
+pip install --upgrade python-tgpt
+pip install curl_cffi
+python3 -m pip install --upgrade pip
+pip install --upgrade pip
 
