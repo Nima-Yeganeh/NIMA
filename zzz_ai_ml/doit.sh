3aea02c3733919d28753de8a1a7e7d3b2527bac4
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-07-19 12:11:54 +0200

    done

--- zzz_ai_ml/doit.sh
+++ zzz_ai_ml/doit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Started..."
 export LC_ALL=en_US.UTF-8
+zkeywords=$(cat zkeywords.txt | head -n1)
+echo "Keywords >> $zkeywords"
+python -m pytgpt generate "i have a course about $zkeywords - give me 60 topics and domains without explanation for students to learn in this course" > zcrsdomains.txt
+grep '^[0-9]\+' zcrsdomains.txt | sed 's/^[0-9]\+\.\s*//' > ztopics.txt
 # Read file into an array
 mapfile -t lines < ztopics.txt
 echo > zzz.txt
