edab470230c19378bd76bfda1c32ddbf04a83ba7
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-07-26 16:42:00 +0200

    done

--- zzz_ai_ml/zmp4make.sh
+++ zzz_ai_ml/zmp4make.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Function to check if input is empty or blank
+is_empty_or_blank() {
+    [[ -z "$1" || "$1" =~ ^[[:space:]]*$ ]]
+}
+# Loop until a non-empty input is received
+while true; do
+    read -p "Enter your filename: " input
+    if ! is_empty_or_blank "$input"; then
+        break
+    else
+        echo "Input cannot be empty or blank. Please try again."
+    fi
+done
 # Source directory path
 src_dir="/c/Users/Nimay/zzz_nimax_files/zzzz_mine_picture_folder_notebook/Screenshots"
 # Destination file path
@@ -20,3 +33,4 @@ fi
 ffmpeg -loop 1 -i "C:\Users\Nimay\Downloads\z.png" -i "C:\Users\Nimay\Downloads\tts_rvc_output.wav" -r 30 -c:v libx264 -tune stillimage -af "volume=10dB" -c:a libmp3lame -b:a 128k -ac 2 -shortest "C:\Users\Nimay\Downloads\zzzxyz.mp4"
 rm -f "C:\Users\Nimay\Downloads\tts_rvc_output.wav"
 rm -f "C:\Users\Nimay\Downloads\z.png"
+mv -f "C:\Users\Nimay\Downloads\zzzxyz.mp4" "C:\\Users\\Nimay\\Downloads\\$input.mp4"
